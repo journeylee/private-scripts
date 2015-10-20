12833d9bdb48e03cad7e9c294c5018676edf269b
李雎 <journeyleee@gmail.com>
2015-10-20 11:31:40 +0800

    add oh-my-zsh custom configurations

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -svnf ${PWD}/vimrc ${HOME}/.vimrc
 ln -svnf ${PWD}/vim ${HOME}/.vim
 ln -svnf ${PWD}/bashrc ${HOME}/.bashrc
 ln -svnf ${PWD}/bash_profile ${HOME}/.bash_profile
+ln -svnf ${PWD}/ohmyzsh-custom ${HOME}/.oh-my-zsh/custom
